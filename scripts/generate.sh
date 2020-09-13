fba768721a6b4526d5e1dbd1f290436ac7dd3e02
rkumagai <adalberyo@gmail.com>
2020-09-13 18:28:45 +0900

    Fixed

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
 # Generate openapi.yaml from root.yaml
-openapi-generator generate -g openapi-yaml -i root.yaml -o openapi
+openapi-generator generate -g openapi-yaml -i root.yaml -o ./
 
 # Generate ruby codes from openapi.yaml
-openapi-generator generate -g ruby -i openapi/openapi/openapi.yaml -o dist
+openapi-generator generate -g ruby -i openapi/openapi.yaml -o dist
 
 # Generate ActiveModel::Serializers from openapi.yaml
-./scripts/openapi2ruby/bin/openapi2ruby generate openapi/openapi/openapi.yaml --out serializers
+./scripts/openapi2ruby/bin/openapi2ruby generate openapi/openapi.yaml --out serializers
